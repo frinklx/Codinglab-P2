b9f1fb9f0f2d7113ab00c5d81a05cec7ba0bcc54
badprawn <160569799+badprawn@users.noreply.github.com>
2025-01-18 16:34:51 +0800

    class3

--- class2.py
+++ class2.py
@@ -1,2 +1,13 @@
 s = "hello world"
 
+print(s[4])
+print(s[2:6])
+
+
+# looping through each char of string
+for char in s:
+    print(char)
+    
+# looping using indeex
+for index in range(len(s)):
+    print(index, s[index])
---
+++ class3.py
@@ -0,0 +1,9 @@
+phonebook = {
+    "John": 1234567890,
+    "Jane": 9876543210,
+    "Jim": 5555555555
+}
+
+for key in phonebook:
+    value = phonebook[key]
+    print(key, " - ", value)
